5e1872efe12652846cab8e1409db854bb82c021b
tdarnett <tarnett1@mac.com>
2016-03-01 19:30:47 -0700

    almost finished Session class

---
+++ TutorTrader/app/src/main/java/com/teamname/tutortrader/InvalidStatusException.java
@@ -0,0 +1,8 @@
+package com.teamname.tutortrader;
+
+/**
+ * Created by taylorarnett on 2016-03-01.
+ */
+public class InvalidStatusException extends Exception {
+    // will be called when status is not set to  "available","bidded", or "booked"
+}
