9f7fa132c32c81ed49cf84760081460eee88b071
Imran Ali <iali1@cs.ualberta.ca>
2016-04-03 13:09:27 -0600

    getting unique usernames

--- TutorTrader/app/src/androidTest/java/com/teamname/tutortrader/AvailableSessionsActivityTest.java
+++ TutorTrader/app/src/androidTest/java/com/teamname/tutortrader/AvailableSessionsActivityTest.java
@@ -5,12 +5,21 @@ import android.app.Instrumentation;
 import android.graphics.Bitmap;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.UiThreadTest;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.robotium.solo.Solo;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import io.searchbox.client.JestResult;
+import io.searchbox.core.Delete;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
 
 /**
  * This will test the addSessionActivity by adding a new activity and see that it has been
@@ -18,7 +27,7 @@ import java.util.ArrayList;
  *
  * @see AddSessionActivity for more details about what it does
  */
-public class ActivitySessionActivityTest extends ActivityInstrumentationTestCase2 {
+public class AvailableSessionsActivityTest extends ActivityInstrumentationTestCase2 {
 
 
     Instrumentation instrumentation;
@@ -43,7 +52,7 @@ public class ActivitySessionActivityTest extends ActivityInstrumentationTestCase
         solo.finishOpenedActivities();
         super.tearDown();
     }
-    public ActivitySessionActivityTest() {
+    public AvailableSessionsActivityTest() {
         super(AvailableSessionsActivity.class);
     }
 
@@ -59,8 +68,8 @@ public class ActivitySessionActivityTest extends ActivityInstrumentationTestCase
     public void testViewAvailable() {
         Profile profile = new Profile("Test tutor", "test@test.test", "780-666-6666");
         Bitmap.Config conf = Bitmap.Config.ARGB_8888;
-        Bitmap bm1 = Bitmap.createBitmap(1,2, conf);
-        Session session = new Session("Math", "Tutor for linear Algebra for all university levels",profile.getProfileID(),bm1 );
+        Bitmap bm1 = Bitmap.createBitmap(1, 2, conf);
+        Session session = new Session("Math", "Tutor for linear Algebra for all university levels", profile.getProfileID(), bm1);
 
         ElasticSearchController.AddProfileTask profileTask = new ElasticSearchController.AddProfileTask();
         profileTask.execute(profile);
@@ -71,17 +80,35 @@ public class ActivitySessionActivityTest extends ActivityInstrumentationTestCase
         assertEquals("this is the title we expected", session.getTitle(), "Math");
         assertEquals("this is the description we expected", session.getDescription(),
                 "Tutor for linear Algebra for all university levels");
-    }
 
-    /**
-     * Testing UseCase 01.04.01 - ViewOneSession
-     * "As an owner, I want to view one of my things, its description and status."
-     * <p/>
-     * We will perform a click on list entry to bring us to the ViewOneSession view.
-     * From here we test to see that all the buttons are present, and all the TextViews are
-     * accurate
-     */
-    public void testViewOneSession() {
+
+        /**
+         * Testing UseCase 04.01.01 and 04.02.01 - Searching
+         * "As a borrower, I want to specify a set of keywords, and search for all things not currently
+         * borrowed whose description contains all keywords." and "As a borrower, I want search results
+         * to show each thing not currently borrowed with its description, owner username, and status."
+         * <p/>
+         * We will type things into the search bar. Then we will click search. This will then
+         * bring up a new screen with the search results.
+         */
+        solo.clickOnMenuItem("Available");
+        solo.assertCurrentActivity("right activity", AvailableSessionsActivity.class);
+
+        solo.typeText(0, "Math");
+        solo.clickOnButton("Search");
+
+        assertTrue(solo.searchText("Math", 2));
+        assertTrue(solo.searchText("Tutor for linear Algebra for all university levels"));
+
+        /**
+         * Testing UseCase 01.04.01 - ViewOneSession
+         * "As an owner, I want to view one of my things, its description and status."
+         * <p/>
+         * We will perform a click on list entry to bring us to the ViewOneSession view.
+         * From here we test to see that all the buttons are present, and all the TextViews are
+         * accurate
+         */
+
         solo.assertCurrentActivity("right activity", AvailableSessionsActivity.class);
 
         solo.clickOnText("Math");
@@ -92,26 +119,28 @@ public class ActivitySessionActivityTest extends ActivityInstrumentationTestCase
         assertTrue(solo.searchText("780-666-6666"));
         assertTrue(solo.searchText("test@test.test"));
 
-    }
 
-    /**
-     * Testing UseCase 04.01.01 and 04.02.01 - Searching
-     * "As a borrower, I want to specify a set of keywords, and search for all things not currently
-     * borrowed whose description contains all keywords." and "As a borrower, I want search results
-     * to show each thing not currently borrowed with its description, owner username, and status."
-     * <p/>
-     * We will type things into the search bar. Then we will click search. This will then
-     * bring up a new screen with the search results.
-     */
-    public void testSearching(){
+        /**
+         *  Testing Use Case 01.05.01 - DeleteSession
+         * "As an owner, I want to delete a thing in my things."
+         *
+         * To test this we create a session, then change the activities to ge to the EditSession
+         * view. The first test case tests if the delete occured when the user confirmed the delete.
+         *
+         * The second test tests the case when the user does not confirm the delete.
+         */
+        solo.clickOnMenuItem("Available");
         solo.assertCurrentActivity("right activity", AvailableSessionsActivity.class);
 
-        solo.typeText(0, "Math");
-        solo.clickOnButton("Search");
+        Profile owner = MethodsController.getProfile(session.getTutorID());
+        ElasticSearchController.RemoveProfileTask removeProfileTask = new ElasticSearchController.RemoveProfileTask();
+        removeProfileTask.execute(owner.getProfileID());
 
-        assertTrue(solo.searchText("Math", 2));
-        assertTrue(solo.searchText("Tutor for linear Algebra for all university levels"));
+        ElasticSearchController.RemoveSessionTask removeSessionTask = new ElasticSearchController.RemoveSessionTask();
+        removeSessionTask.execute(session.getSessionID());
+
+        assertFalse(solo.searchText("Math"));
+
     }
-
 
 }
